da537ba4602bbafad57df03e74dcbbd240171e1b
Colin Prepscius <colinprepscius@gmail.com>
2019-02-04 15:54:56 -0500

    Changed representation of LIFNeuron from RC to R\tau.

--- core/neuron_layer.py
+++ core/neuron_layer.py
@@ -44,23 +44,28 @@ class LIFNeuronLayer(NeuronLayer):
     Contains multiple convenience creation functions.
 
     Configuration values can be different for each neuron:
-        r: resistance
-        c: capacitance
-        t: threshhold
-        refrac: refactory periods in number of timesteps
+        resistance: np.array(floats)
+        tau: time constant, np.array(floats)
+        treshhold: threshhold, np.array(floats)
+        n_refrac: refactory periods in number of timesteps, np.array(ints)
     """
 
-    C = namedtuple('LIFNeuronConfig', ['r', 'c', 't', 'refrac'])
+    C = namedtuple('LIFNeuronConfig', ['resistance', 'tau', 'threshhold', 'n_refrac'])
 
     def __init__(self, neuron_configuration, dt=0.5):
+        """ LIFNeuronLayer constructor
+        Args:
+            neuron_configuration: 2d numpy array; columns are
+            resistance, tau, capacitance, n_refrac
+            dt: single timestep dt value.
+        """
         super().__init__(neuron_configuration.shape[0])
 
-        self.r = neuron_configuration[:,0] # resistance
-        self.c = neuron_configuration[:,1] # capacitance
-        self.t = neuron_configuration[:,2] # threshhold
-        self.refrac = neuron_configuration[:,3].astype(np.int32) # refractory periods
+        self.resistance = neuron_configuration[:,0]
+        self.tau = neuron_configuration[:,1]
+        self.threshhold = neuron_configuration[:,2]
+        self.n_refrac = neuron_configuration[:,3].astype(np.int32)
         self.dt = np.float32(dt)
-        self.tau = self.r * self.c
 
     @classmethod
     def layer_from_tuples(cls, neuron_configuration_tuples, dt=0.5):
@@ -69,81 +74,78 @@ class LIFNeuronLayer(NeuronLayer):
             neuron_configuration_tuples: [LIFNeuronLayer.C]
             dt: state update timestep, float
         """
-        r = np.array([nn.r for nn in neuron_configuration_tuples], dtype=np.float32)
-        c = np.array([nn.c for nn in neuron_configuration_tuples], dtype=np.float32)
-        t = np.array([nn.t for nn in neuron_configuration_tuples], dtype=np.float32)
-        refrac = np.array([nn.refrac for nn in neuron_configuration_tuples], dtype=np.float32)
-        configuration = np.array([r, c, t, refrac]).T
+        nct = neuron_configuration_tuples
+        res = np.array([nn.resistance for nn in nct], dtype=np.float32)
+        tau = np.array([nn.tau for nn in nct], dtype=np.float32)
+        thresh = np.array([nn.threshhold for nn in nct], dtype=np.float32)
+        refrac = np.array([nn.n_refrac for nn in nct], dtype=np.float32)
+        configuration = np.array([res, tau, thresh, refrac]).T
         return cls(configuration, dt)
 
     @classmethod
-    def layer_with_n_identical_neurons(cls, n, r, c, t, refrac, dt):
+    def layer_with_n_identical_neurons(cls, n, resistance, tau, threshhold, n_refrac, dt):
         """ Creates a layer of n identical neurons.
         Args:
-            r, c, t, refrac: floats
+            resistance, tau, threshhold, n_refrac: floats
             dt: state update timestep, float
         """
         configuration = np.tile(np.array([r, c, t, refrac]), (n,1))
         return cls(configuration, dt)
 
     @classmethod
-    def layer_with_n_distributions(cls, n, r_dist, c_dist, t_dist, refrac_dist, dt):
+    def layer_with_n_distributions(cls, n, resistance_dist, tau_dist, threshhold_dist, n_refrac_dist, dt):
         """ Creates a layer of n neurons, whose configuration values are pulled from
         distribution generation functions.
         Args:
-            r_dist, c_dist, t_dist, refrac_dist: functions that generate n float values according to any distribution.
+            *_dist: functions that generate n float values according to any distribution.
             dt: state update timestep, float
         """
-        configuration = np.array([r_dist(n), c_dist(n), t_dist(n), refrac_dist(n)]).T
+        configuration = np.array([resistance_dist(n), tau_dist(n), threshhold_dist(n), n_refrac_dist(n)]).T
         return cls(configuration, dt)
 
     def to_dataframe(self):
         """ Returns a pandas Dataframe containing the neuron configurations.
         """
-        return pd.DataFrame(np.array([self.r, self.c, self.t, self.refrac]).T,
-                            index = range(len(self.r)),
-                            columns = ['r', 'c', 't', 'refrac'])
+        return pd.DataFrame(np.array([self.resistance, self.tau, self.threshhold, self.n_refrac]).T,
+                            index = range(len(self.resistance)),
+                            columns = ['resistance', 'tau', 'threshhold', 'n_refrac'])
 
 
     def _ops(self):
-        return [self.input, self.fired_op, self.recovery_op, self.v_op]
+        return [self.input, self.recovery_op, self.fired_op, self.v_op]
 
     def _compile(self):
         nintzeros = np.zeros((self.n,), dtype=np.int32)
         nfloatzeros = np.zeros((self.n,), dtype=np.float32)
 
-        # refractory recovery times
-        self.recovery_times = tf.Variable(nintzeros, dtype=tf.int32, name='recoverytimes')
-
-        # Create tensor variables for the neuron states
+        # internal tensor variables for the neuron states
         v = tf.Variable(nfloatzeros, name='v')
-        self.output = tf.Variable(np.zeros(v.shape, dtype=bool), dtype=bool, name='fired')
+        recovery_times = tf.Variable(nintzeros, dtype=tf.int32, name='recoverytimes')
+
+        # define output variable
+        self.output = tf.Variable(np.zeros(nfloatzeros.shape, dtype=bool), dtype=bool, name='fired')
 
         # Reset any neurons that spiked last timestep
         # NOTE: Does the WARNING: tf.Variable from the tensorflow Variable
         # documentation apply here? Is tf.where safe here, and below?
         v_1 = tf.where(self.output, nfloatzeros, v)
 
+        # Reset recovery durations for neurons that spiked last timestep.
+        recovery_decremented = tf.maximum(nintzeros, recovery_times - 1)
+        recovery_new = tf.where(self.output, self.n_refrac, recovery_decremented)
+        self.recovery_op = recovery_times.assign(recovery_new)
+
         # State update equations: update the 'voltage', but
         # only do it if the neuron is not in its recovery period.
-        v_2 = v_1 + (self.input * self.r - v_1) / self.tau * self.dt
-        recovering = tf.greater(self.recovery_times, nintzeros)
+        v_2 = v_1 + (self.input * self.resistance - v_1) / self.tau * self.dt
+        recovering = tf.greater(self.recovery_op, nintzeros)
         v_3 = tf.where(recovering, nfloatzeros, v_2)
 
-        # Spikes:
-        # Limit anything above threshold to threshold value
-        # We are saving which fired to use again in the next iteration
-        threshhold = tf.constant(self.t, tf.float32, v.shape)
-        self.fired_op = self.output.assign(tf.greater_equal(v_3, threshhold))
-        v_f = tf.where(self.fired_op, threshhold, v_3)
+        # Compute spikes that fired, assign to output
+        self.fired_op = self.output.assign(tf.greater_equal(v_3, self.threshhold))
 
-        # Set the new recovery time - if the neuron fired, set to the refactory
-        # period - otherwise decrement by one (floor of 0).
-        recovery_decremented = tf.maximum(nintzeros, self.recovery_times - 1)
-        recovery_new = tf.where(self.output, self.refrac, recovery_decremented)
-        self.recovery_op = self.recovery_times.assign(recovery_new)
-
-        # Operations to update the state
+        # Update the state: where spiked, set to threshhold
+        v_f = tf.where(self.fired_op, self.threshhold, v_3)
         self.v_op = v.assign(v_f)
 
 
@@ -161,7 +163,7 @@ class IzhikevichNeuronLayer(NeuronLayer):
         Args:
             neuron_configuration: np.array of size n by 6. Each row represents
             one neuron. Columns 0 through 5 represent values of a, b, c, d, t, and v0.
-            dt: python float value for dt.
+            dt: state update timestep, float
         """
         super().__init__(neuron_configuration.shape[0])
 
